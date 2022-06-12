77e14bda80fc2af1c2066e11052ec1c0959c1cd2
Chris Castle <chrisjcastle93@gmail.com>
2022-06-12 11:41:37 +0200

    L5 completed

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [{name:'Small', quantity: 4}, {name:'Medium', quantity: 2}, {name:'Large', quantity: 2}]
         }
     }
 })
