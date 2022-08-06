905ce5841a3f8a7464d99a0013eb2964655f0584
Chris Castle <chris.castle@getyourguide.com>
2022-08-06 18:24:45 +0200

    Completed Intro to Vue3

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: [],
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+      reviews: []
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {
-        updateCart(id) {
-            this.cart.push(id)
-        }
-    }
-  })
+
+    addReview(review) {
+      this.reviews.push(review);
+    },
+  },
+});
   
